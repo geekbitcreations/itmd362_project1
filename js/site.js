ad2e4973d4485e9e5272edb9f14f4977a6db2349
geekbitcreations <dbarndt@hawk.iit.edu>
2018-02-19 10:37:40 -0600

    changing name to id and adding some javascript

--- js/site.js
+++ js/site.js
@@ -3,3 +3,38 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+<script type="text/javascript">
+		function validateInfo()
+		{
+			var name = document.getElementById("fname");
+			var ssn = document.getElementById("lname");
+			var email = document.getElementById("email");
+			
+			if (document.clubinfo.fname.value == "" ||
+					document.clubinfo.fname.value < 4 ||
+					document.clubinfo.fname.value > 50)
+			{
+				alert("Please provide your first name, and must be between 4-50 characters.");
+				document.clubinfo.fname.focus();
+				return false;
+			}
+      
+      if (document.clubinfo.lname.value == "" ||
+					document.clubinfo.lname.value < 4 ||
+					document.clubinfo.lname.value > 50)
+			{
+				alert("Please provide your last name, and must be between 4-50 characters.");
+				document.clubinfo.lname.focus();
+				return false;
+			}
+			
+			if (document.clubinfo.email.value == "" ||
+          document.clubinfo.email.value != "@")
+			{
+				alert("Please provide your email address.");
+				document.clubinfo.email.focus();
+				return false;
+			}
+		}
+	</script>

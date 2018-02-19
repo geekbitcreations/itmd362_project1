a61f95ee536a333940dc0c035c609512c14cf8ea
geekbitcreations <dbarndt@hawk.iit.edu>
2018-02-19 10:56:22 -0600

    changing javascript code variable names

--- js/site.js
+++ js/site.js
@@ -4,37 +4,38 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
-<script type="text/javascript">
-		function validateInfo()
-		{
-			var name = document.getElementById("fname");
-			var ssn = document.getElementById("lname");
-			var email = document.getElementById("email");
-			
-			if (document.clubinfo.fname.value == "" ||
-					document.clubinfo.fname.value < 4 ||
-					document.clubinfo.fname.value > 50)
-			{
-				alert("Please provide your first name, and must be between 4-50 characters.");
-				document.clubinfo.fname.focus();
-				return false;
-			}
-      
-      if (document.clubinfo.lname.value == "" ||
-					document.clubinfo.lname.value < 4 ||
-					document.clubinfo.lname.value > 50)
-			{
-				alert("Please provide your last name, and must be between 4-50 characters.");
-				document.clubinfo.lname.focus();
-				return false;
-			}
-			
-			if (document.clubinfo.email.value == "" ||
-          document.clubinfo.email.value != "@")
-			{
-				alert("Please provide your email address.");
-				document.clubinfo.email.focus();
-				return false;
-			}
-		}
-	</script>
+
+// Function to validate that the information in the form is filled out.
+function validateInfo()
+{
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var email = document.getElementById("email");
+  
+  if (document.clubinfo.fname.value == "" ||
+      document.clubinfo.fname.value < 4 ||
+      document.clubinfo.fname.value > 50)
+  {
+    alert("Please provide your first name, and must be between 4-50 characters.");
+    document.clubinfo.fname.focus();
+    return false;
+  }
+  
+  if (document.clubinfo.lname.value == "" ||
+      document.clubinfo.lname.value < 4 ||
+      document.clubinfo.lname.value > 50)
+  {
+    alert("Please provide your last name, and must be between 4-50 characters.");
+    document.clubinfo.lname.focus();
+    return false;
+  }
+  
+  if (document.clubinfo.email.value == "" ||
+      document.clubinfo.email.value != "@")
+  {
+    alert("Please provide your email address.");
+    document.clubinfo.email.focus();
+    return false;
+  }
+}
+

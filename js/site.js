039b4200f47c2a8b7d526b776d4216fba8efb8c6
geekbitcreations <dbarndt@hawk.iit.edu>
2018-03-05 20:51:27 -0600

    fixing my javascript

--- js/site.js
+++ js/site.js
@@ -10,13 +10,18 @@ function confirm(event){
   for(var i = 0; i < document.getElementsByTagName("input").length; i++)
   {
     if(document.getElementsByTagName("input").item(i).value === "" ||
-        (document.getElementsByTagName("input").item(i).type === "radio") && 
+        (document.getElementsByTagName("input").item(i).type === "radio") &&
         (document.getElementsByTagName("input").item(i).checked === false))
         {
           return false;
-        }    
+        } 
+        
+        else 
+        {
+          alert("Thank you " + fname + " " + lname + " for signing up for our newsletter!");
+          return true;
+        }
   }
-  
-  alert("Thank you " + fname + " " + lname + " for signing up for our newsletter!");
-  return true;
-});
+}
+
+
